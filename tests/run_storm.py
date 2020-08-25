fbb5a3802a087e3c7296501d5ef46b381c8c2db4
GilmoreDM <34312704+GilmoreDM@users.noreply.github.com>
2020-08-25 10:45:40 -0700

    Add files via upload

--- tests/run_storm.py
+++ tests/run_storm.py
@@ -1,26 +1,37 @@
 #!/usr/bin/env python3
 
 import sys
+import os
 import tropycal.tracks as tracks
 import datetime as dt
 
 tstorm = sys.argv[1]
-if 'L' in tstorm:
-    tstorm = tstorm[:tstorm.index('L')]
+image_path = sys.argv[2]
+tc_name = sys.argv[3]
 
-if not tstorm.isnumeric():
-    print("Usage: $ run_storm.py nn\nwhere 'nn' is the storm number")
-    raise SystemExit
+if tstorm[-1] == 'L':
+    tstorm_num = tstorm[:tstorm.index('L')]
+    try:
+        nrldat_atl = tracks.NRLTrackDataset(basin='north_atlantic',source='nrlcotc',include_btk=False,storm=f'{tstorm_num}L',image_path=image_path,tc_name=tc_name)
+    except tracks.nrl_dataset.MissingData:
+        print(f"Could not find data for storm {tstorm}L")
+        raise SystemExit
+elif not tstorm.isnumeric():
+    if os.path.exists(tstorm):
+        try:
+            nrldat_atl = tracks.NRLTrackDataset(basin='north_atlantic',source='nrlcotc',include_btk=False,datafile=f'{tstorm}',image_path=image_path,tc_name=tc_name)
+        except tracks.nrl_dataset.MissingData:
+            print(f"Could not use data file {tstorm} for storm")
+            raise SystemExit
+        tstorm_num = tstorm[tstorm.index('_')+1:tstorm.index('L.')]
+    else:
+        print("Could not find datafile for storm")
+        raise SystemExit
 
-try:
-    nrldat_atl = tracks.NRLTrackDataset(basin='north_atlantic',source='nrlcotc',include_btk=False,storm=f'{tstorm}L')
-except tracks.nrl_dataset.MissingData:
-    print(f"Could not find data for storm {tstorm}L")
-    raise SystemExit
 
 if isinstance(nrldat_atl,tracks.NRLTrackDataset):
-    storm = nrldat_atl.get_storm((f'AL{tstorm}2020'))
-    storm.plot_nrl_forecast(forecast=dt.datetime(2020, 8, 25, 0, 0))
+    storm = nrldat_atl.get_storm((f'AL{tstorm_num}2020'))
+    storm.plot_nrl_forecast(forecast=dt.datetime(2020, 8, 25, 0, 0),image_path=image_path)
 else:
     print(f"Could not find dataset for {tstorm}L")
 
